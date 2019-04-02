74b3d2faced23dcf8bef41832d8bdace559fb333
Jaap Joris Vens <jj@rtts.eu>
2019-04-02 16:19:43 +0200

    include subpackages

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'django-simplecms',
-    version = '0.0.1',
+    version = '0.0.2',
     url = 'https://github.com/rtts/django-simplecms',
     author = 'Jaap Joris Vens',
     author_email = 'jj@rtts.eu',
     license = 'GPL3',
-    packages = ['cms'],
+    packages = find_packages(),
     install_requires = [
         'django',
         'django-ckeditor',
