b106d740a0e8154d02b2abf261235e90ee8823fc
Jaap Joris Vens <jj@rtts.eu>
2019-04-03 15:50:36 +0200

    update translations

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-simplecms',
-    version = '0.0.4',
+    version = '0.0.5',
     url = 'https://github.com/rtts/django-simplecms',
     author = 'Jaap Joris Vens',
     author_email = 'jj@rtts.eu',
