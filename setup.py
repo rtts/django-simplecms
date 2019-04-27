96e47cc27774b056ffc805530b31965bc01335c0
Jaap Joris Vens <jj@rtts.eu>
2019-04-27 13:06:23 +0200

    only add sections to existing pages to work around formset bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-simplecms',
-    version = '0.0.7',
+    version = '0.0.8',
     url = 'https://github.com/rtts/django-simplecms',
     author = 'Jaap Joris Vens',
     author_email = 'jj@rtts.eu',
