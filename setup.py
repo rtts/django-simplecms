f7e4b1066d5de5fbf54345b13c8cc1718dac595e
Jaap Joris Vens <jj@rtts.eu>
2019-04-29 13:13:54 +0200

    hook for extending base sections

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-simplecms',
-    version = '0.0.8',
+    version = '0.0.9',
     url = 'https://github.com/rtts/django-simplecms',
     author = 'Jaap Joris Vens',
     author_email = 'jj@rtts.eu',
