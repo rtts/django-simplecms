102a2288bfa7e22f75ee73422d295b7d00ec3c76
Jaap Joris Vens <jj@rtts.eu>
2019-08-27 14:28:14 +0200

    first real release! \o/

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-simplecms',
-    version = '0.0.9',
+    version = '1.0',
     url = 'https://github.com/rtts/django-simplecms',
     author = 'Jaap Joris Vens',
     author_email = 'jj@rtts.eu',
